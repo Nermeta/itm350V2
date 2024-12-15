0d1ff3a91edd20c0e599a6f29833221290b71c99
Wynter Gailey <56845892+Nermeta@users.noreply.github.com>
2024-12-14 22:57:02 -0700

    Updated js to support Dynamo DB

--- bb/backend/api.js
+++ bb/backend/api.js
@@ -1,9 +1,59 @@
-var events = require('./events.js');
+const statusCodes = require('http').STATUS_CODES;
+const httpConstants = require('http2').constants;
+// Import DynamoDB document client from aws.js
+const { docClient, getCommand, deleteCommand, putCommand, scanCommand } = require("./aws");
 
 exports.events = function (req, res) {
-  res.json(events);
+  let id = req.query.id;
+  if (id) {
+    getCommand.input.Key.id = Number(id);
+    docClient.send(getCommand).then(
+      (data) => {
+        res.send(data.Item);
+      },
+      (error) => {
+        console.error(error);
+        res.send(error);
+      }
+    );
+  } else {
+    docClient.send(scanCommand).then(
+      (data) => {
+        res.json(data.Items);
+      },
+      (error) => {
+        console.error(error);
+        res.send(error);
+      }
+    );
+  }
 };
 
 exports.event = function (req, res) {
-  res.json(events[req.param.eventId]);
+  let body = req.body;
+  let id = req.params.eventId;
+  if (id && req.method === httpConstants.HTTP2_METHOD_DELETE) {
+    deleteCommand.input.Key.id = Number(id);
+    docClient.send(deleteCommand).then(
+      (data) => {
+        res.send(data);
+      },
+      (error) => {
+        console.error("Delete error:", error);
+        res.send(error);
+      }
+    );
+  } else if (req.method === httpConstants.HTTP2_METHOD_POST && body.title.trim()) {
+    body.id = Date.now();
+    putCommand.input.Item = body;
+    docClient.send(putCommand).then(
+      (data) => {
+        res.send(data);
+      },
+      (error) => {
+        console.error("Put error:", error);
+        res.send(error);
+      }
+    );
+  }
 };
